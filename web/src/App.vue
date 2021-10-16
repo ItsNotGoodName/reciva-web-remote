025e667417fb12725e72ed5c40be58e09785f8f0
ItsNotGoodName <gurnaindeol@gmail.com>
2021-10-15 22:27:52 -0700

    feat(web): add border and title

--- web/src/App.vue
+++ web/src/App.vue
@@ -11,7 +11,7 @@ export default {
   components: {
     SelectRadio,
     RadioPanel,
-    RadioPresets, 
+    RadioPresets,
     RadioStatus
   },
   setup() {
@@ -39,7 +39,7 @@ export default {
   <RadioStatus v-if="state.uuid" :connecting="state.connecting" :connected="state.connected" />
   <div class="container mx-auto px-2">
     <SelectRadio
-      class="mt-2"
+      class="mt-2 pb-2 border-b-2"
       :uuid="state.uuid"
       :radios="state.radios"
       :selectRadio="selectRadio"
@@ -47,7 +47,7 @@ export default {
     />
     <div v-if="state.uuid">
       <RadioPanel
-        class="mt-2"
+        class="mt-2 pb-2 border-b-2"
         :radio="state.radio"
         :toggleRadioPower="toggleRadioPower"
         :setRadioVolume="setRadioVolume"

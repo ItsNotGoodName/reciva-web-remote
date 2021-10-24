245dd8bfffa630753fa0f147c82eb715b3de30ae
ItsNotGoodName <gurnaindeol@gmail.com>
2021-10-24 01:56:56 -0700

    test: add GetTest

--- store/store_test.go
+++ store/store_test.go
@@ -4,6 +4,7 @@ import (
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"reflect"
 	"testing"
 
 	"github.com/ItsNotGoodName/reciva-web-remote/config"
@@ -15,6 +16,16 @@ func TestStore(t *testing.T) {
 	// Test canceling context right after writing settings
 	s.WriteSettings()
 	s.Cancel()
+
+	// Compare saved settings to current settings
+	sg, err := s.readSettings()
+	if err != nil {
+		t.Fatalf("TestStore(err) = %s, want nil", err)
+	}
+	if !reflect.DeepEqual(*sg, *s.sg) {
+		t.Fatalf("TestStore(sg) = %+v, want %+v", *sg, *s.sg)
+	}
+
 }
 
 func testStore(t *testing.T) *Store {
@@ -23,11 +34,17 @@ func testStore(t *testing.T) *Store {
 	if err != nil {
 		t.Fatal(err)
 	}
-	cfg := config.NewConfig()
-	cfg.ConfigPath = filepath.Join(p, "settings.json")
+	cfg := config.Config{
+		Port:          0,
+		CPort:         0,
+		ConfigPath:    filepath.Join(p, "settings.json"),
+		EnablePresets: true,
+		Presets:       []string{"/01.m3u", "/02.m3u"},
+		APIURI:        "",
+	}
 
 	// Store should be created
-	s, err := NewService(cfg)
+	s, err := NewService(&cfg)
 	if err != nil {
 		t.Fatal(err)
 	}

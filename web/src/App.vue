277ed156dde4a3b3c7a01bdf6efa071df0f69f1c
ItsNotGoodName <gurnaindeol@gmail.com>
2021-10-26 16:01:26 -0700

    feat(web): add vuex

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,64 +1,29 @@
 <script>
-import SelectRadio from './components/SelectRadio.vue';
-import RadioPanel from './components/RadioPanel.vue';
-import RadioPresets from './components/RadioPresets.vue';
-import RadioStatus from './components/RadioStatus.vue';
-
-import Store from "./store"
+import { mapActions } from 'vuex';
 
 export default {
-  name: "App",
-  components: {
-    SelectRadio,
-    RadioPanel,
-    RadioPresets,
-    RadioStatus
-  },
-  setup() {
-    return {
-      state: Store.getState(),
-    }
-  },
-  methods: {
-    updateRadios: () => Store.updateRadios()
-  },
-  mounted() {
-    Store.updateRadios()
-  },
-  methods: {
-    selectRadio: (uuid) => Store.selectRadio(uuid),
-    setRadioPreset: (preset) => Store.setRadioPreset(preset),
-    setRadioVolume: (volume) => Store.setRadioVolume(volume),
-    discoverRadios: () => Store.discoverRadios(),
-    toggleRadioPower: () => Store.toggleRadioPower(),
-    refreshRadioVolume: () => Store.refreshRadioVolume(),
-    renewRadio: ()=> Store.renewRadio()
-  }
+	name: "App",
+	components: { },
+	mounted() {
+		this.loadAll()
+	},
+	computed: {
+		state() {
+			return this.$store.state
+		}
+	},
+	methods: {
+		...mapActions([
+			'loadAll'
+		])
+	}
 }
 </script>
 
 <template>
-  <RadioStatus v-if="state.uuid" :connecting="state.connecting" :connected="state.connected" />
-  <div class="container mx-auto px-2">
-    <SelectRadio
-      class="mt-2 pb-2 border-b-2"
-      :uuid="state.uuid"
-      :radios="state.radios"
-      :selectRadio="selectRadio"
-      :discoverRadios="discoverRadios"
-    />
-    <div v-if="state.uuid">
-      <RadioPanel
-        class="mt-2 pb-2 border-b-2"
-        :radio="state.radio"
-        :toggleRadioPower="toggleRadioPower"
-        :setRadioVolume="setRadioVolume"
-        :refreshRadioVolume="refreshRadioVolume"
-        :renewRadio="renewRadio"
-      />
-      <RadioPresets class="mt-2" :radio="state.radio" :setRadioPreset="setRadioPreset" />
-    </div>
-  </div>
+	<div class="container mx-auto px-2">
+		<pre><code>{{ JSON.stringify(state, null, 2) }}</code></pre>
+	</div>
 </template>
 
 <style>
--- web/src/store.js
+++ web/src/store.js
@@ -1,156 +1,70 @@
-import { reactive, readonly } from "vue";
+import { createStore } from 'vuex'
+import api from './api'
 
-const API_URL = import.meta.env.VITE_API_URL
-  ? import.meta.env.VITE_API_URL
-  : "";
-const WS_URL = import.meta.env.VITE_WS_URL ? import.meta.env.VITE_WS_URL : (() => {
-  if (window.location.protocol == "http:") {
-    return "ws://" + window.location.host
-  }
-  return "wss://" + window.location.host
-})();
-
-export default {
-  state: reactive({
-    connecting: false,
-    connected: false,
-    uuid: "",
-    radio: {},
-    radios: {},
-  }),
-  getState() {
-    return readonly(this.state);
-  },
-  updateRadios() {
-    return fetch(API_URL + "/v1/radios")
-      .then((res) => {
-        if (!res.ok) {
-          throw Error(res.statusText);
-        }
-        return res.json();
-      })
-      .then((data) => {
-        let radios = {};
-        for (let d in data) {
-          radios[data[d].uuid] = data[d].name;
-        }
-        this.state.radios = radios;
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
-  discoverRadios() {
-    fetch(API_URL + "/v1/radios", {
-      method: "POST",
-    })
-      .then(() => {
-        this.updateRadios();
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-  },
-  renewRadio(){
-    if (!this.state.uuid) return;
-    fetch(API_URL + "/v1/radio/" + this.state.radio.uuid + "/renew", {
-      method: "POST",
-    }).catch((error) => {
-      console.log(error);
-    });
-  },
-  refreshRadioVolume(){
-    if (!this.state.uuid) return;
-    fetch(API_URL + "/v1/radio/" + this.state.radio.uuid + "/volume", {
-      method: "POST",
-    }).catch((error) => {
-      console.log(error);
-    });
-  },
-  toggleRadioPower() {
-    if (!this.state.uuid) return;
-    fetch(API_URL + "/v1/radio/" + this.state.radio.uuid, {
-      method: "PATCH",
-      body: JSON.stringify({ power: !this.state.radio.power }),
-    }).catch((error) => {
-      console.log(error);
-    });
-  },
-  setRadioPreset(preset) {
-    if (!this.state.uuid) return;
-    fetch(API_URL + "/v1/radio/" + this.state.radio.uuid, {
-      method: "PATCH",
-      body: JSON.stringify({ preset: preset }),
-    }).catch((error) => {
-      console.log(error);
-    });
-  },
-  setRadioVolume(volume) {
-    if (!this.state.uuid) return;
-    fetch(API_URL + "/v1/radio/" + this.state.radio.uuid, {
-      method: "PATCH",
-      body: JSON.stringify({ volume: volume }),
-    }).catch((error) => {
-      console.log(error);
-    });
-  },
-  selectRadio(uuid = "") {
-    this.state.uuid = uuid;
-    if (!this.initWS()) return;
-    this.ws.send(uuid);
-  },
-  initWS() {
-    if (this.state.connecting) {
-      return false;
-    }
-    if (this.state.connected) {
-      return true;
-    }
-    this.state.connecting = true;
-
-    // Create websocket
-    if (this.state.uuid == undefined || this.state.uuid == "") {
-      this.ws = new WebSocket(WS_URL + "/v1/radio/ws");
-    } else {
-      this.ws = new WebSocket(WS_URL + "/v1/radio/" + this.state.uuid + "/ws");
-    }
-
-    // Handle radio state message
-    this.ws.addEventListener(
-      "message",
-      function (event) {
-        let radio = JSON.parse(event.data);
-        if (radio.uuid != this.state.uuid) return;
-        this.state.radio = radio;
-      }.bind(this)
-    );
-
-    // Handle open
-    this.ws.addEventListener(
-      "open",
-      function () {
-        this.state.connected = true;
-        this.state.connecting = false;
-      }.bind(this)
-    );
-
-    // Handle close
-    this.ws.addEventListener(
-      "close",
-      function (event) {
-        this.state.connected = false;
-        this.state.connecting = false;
-      }.bind(this)
-    );
-
-    // Handle error
-    this.ws.addEventListener(
-      "error",
-      function (event) {
-        this.state.connected = false;
-        this.state.connecting = false;
-        console.error(event);
-      }.bind(this)
-    );
-  },
-};
+export default createStore({
+	state() {
+		return {
+			config: null,
+			presets: null,
+			streams: null,
+			radios: null
+		}
+	},
+	mutations: {
+		setConfig(state, config) {
+			state.config = config
+		},
+		setStreams(state, streams) {
+			state.streams = streams
+		},
+		setPresets(state, presets) {
+			state.presets = presets
+		},
+		setRadios(state, radios) {
+			let rds = {};
+			for (let r in radios) {
+				rds[radios[r].uuid] = radios[r].name;
+			}
+			state.radios = rds;
+		},
+	},
+	actions: {
+		loadAll({ dispatch, state }) {
+			return dispatch('loadConfig').then(() => {
+				dispatch('loadRadios')
+				dispatch('loadPresets')
+				dispatch('loadStreams')
+			})
+		},
+		loadConfig({ commit }) {
+			return api.getConfig()
+				.then((config) => {
+					commit("setConfig", config)
+				})
+		},
+		loadRadios({ commit }) {
+			return api.getRadios()
+				.then((radios) => {
+					commit("setRadios", radios)
+				})
+		},
+		loadStreams({ commit, state }) {
+			if (!state.config.presetsEnabled) {
+				return Promise.resolve()
+			}
+			return api.getStreams()
+				.then((streams) => {
+					commit("setStreams", streams)
+				})
+		},
+		loadPresets({ commit, state }) {
+			if (!state.config.presetsEnabled) {
+				return Promise.resolve()
+			}
+			return api.getPresets()
+				.then((presets) => {
+					commit("setPresets", presets)
+				})
+		}
+	}
+})

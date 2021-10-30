8095a2c68562633967cb9d7fe0aaa52e7c18eeec
ItsNotGoodName <gurnaindeol@gmail.com>
2021-10-30 00:19:52 -0700

    feat(web): add vite-plugin-pwa

--- routes/web.go
+++ routes/web.go
@@ -11,14 +11,6 @@ import (
 func AddWebRoutes(r *gin.Engine, dist *embed.FS) {
 	httpfs := http.FS(dist)
 
-	r.GET("/assets/*assets", func(c *gin.Context) {
-		c.FileFromFS(path.Join("web/dist", c.Request.URL.Path), httpfs)
-	})
-
-	r.GET("/favicon.ico", func(c *gin.Context) {
-		c.FileFromFS("web/dist/favicon.ico", httpfs)
-	})
-
 	index, err := dist.ReadFile("web/dist/index.html")
 	if err != nil {
 		panic(err)
@@ -26,5 +18,45 @@ func AddWebRoutes(r *gin.Engine, dist *embed.FS) {
 
 	r.GET("/", func(c *gin.Context) {
 		c.Writer.Write(index)
+	})
+
+	r.GET("/assets/*assets", func(c *gin.Context) {
+		c.FileFromFS(path.Join("web/dist", c.Request.URL.Path), httpfs)
+	})
+
+	r.GET("/apple-touch-icon.png", func(c *gin.Context) {
+		c.FileFromFS("web/dist/apple-touch-icon.png", httpfs)
+	})
+
+	r.GET("/favicon.ico", func(c *gin.Context) {
+		c.FileFromFS("web/dist/favicon.ico", httpfs)
+	})
+
+	r.GET("/manifest.webmanifest", func(c *gin.Context) {
+		c.FileFromFS("web/dist/manifest.webmanifest", httpfs)
+	})
+
+	r.GET("/pwa-192x192.png", func(c *gin.Context) {
+		c.FileFromFS("web/dist/pwa-192x192.png", httpfs)
+	})
+
+	r.GET("/pwa-512x512.png", func(c *gin.Context) {
+		c.FileFromFS("web/dist/pwa-512x512.png", httpfs)
+	})
+
+	r.GET("/registerWS.js", func(c *gin.Context) {
+		c.FileFromFS("web/dist/registerWS.js", httpfs)
+	})
+
+	r.GET("/robots.txt", func(c *gin.Context) {
+		c.FileFromFS("web/dist/robots.txt", httpfs)
+	})
+
+	r.GET("/sw.js", func(c *gin.Context) {
+		c.FileFromFS("web/dist/sw.js", httpfs)
+	})
+
+	r.GET("/workbox-afb9f189.js", func(c *gin.Context) {
+		c.FileFromFS("web/dist/workbox-afb9f189.js", httpfs)
 	})
 }
--- web/vite.config.js
+++ web/vite.config.js
@@ -1,7 +1,37 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+	plugins: [
+		vue(),
+		VitePWA({
+			includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
+			manifest: {
+				name: 'Reciva Web Remote',
+				short_name: 'Remote',
+				description: 'Control Reciva based radios',
+				theme_color: '#ffffff',
+				icons: [
+					{
+						src: 'pwa-192x192.png',
+						sizes: '192x192',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+					},
+					{
+						src: 'pwa-512x512.png',
+						sizes: '512x512',
+						type: 'image/png',
+						purpose: 'any maskable',
+					}
+				]
+			}
+		})
+	]
 })

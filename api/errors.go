df61f816c5eefde8c630f4c5dbfad0f09d29eab7
ItsNotGoodName <gurnaindeol@gmail.com>
2021-11-08 20:59:28 -0800

    refactor: remove unused error

--- api/errors.go
+++ api/errors.go
@@ -4,4 +4,3 @@ import "errors"
 
 var ErrPresetNewNameInvalid = errors.New("preset new name invalid")
 var ErrPresetNotFound = errors.New("preset not found")
-var ErrNameAlreadyExists = errors.New("name already exists")

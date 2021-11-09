21d282903886c51db5caacaf4a531f49c90bf48c
ItsNotGoodName <gurnaindeol@gmail.com>
2021-11-08 20:58:00 -0800

    feat: allow empty preset new name

--- api/errors.go
+++ api/errors.go
@@ -2,6 +2,5 @@ package api
 
 import "errors"
 
-var ErrPresetNewNameInvalid = errors.New("preset new name invalid")
 var ErrPresetNotFound = errors.New("preset not found")
 var ErrNameAlreadyExists = errors.New("name already exists")

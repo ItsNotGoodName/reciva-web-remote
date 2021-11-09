c57334083d303a16910b394394a5472b44daf936
ItsNotGoodName <gurnaindeol@gmail.com>
2021-11-08 22:03:09 -0800

    refactor: switch to primevue + primeflex

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,38 +1,10 @@
-<script>
-import { mapState } from "vuex";
-
-import Alerts from "./components/Alerts.vue";
-import Player from "./components/Player.vue";
-import PresetPlayer from "./components/PresetPlayer.vue";
-import PresetEditor from "./components/PresetEditor.vue";
-
-export default {
-  name: "App",
-  components: {
-    Alerts,
-    Player,
-    PresetPlayer,
-    PresetEditor,
-  },
-  computed: {
-    ...mapState(["edit"]),
-  },
-  mounted() {
-    this.$store
-      .dispatch("init")
-      .catch((err) =>
-        this.$store.dispatch("addNotification", { type: "error", message: err })
-      );
-  },
-};
+<script setup>
+import Button from "primevue/button";
 </script>
 
-<template >
-  <div class="container mx-auto px-1 h-screen">
-    <Player />
-    <PresetEditor v-if="edit" />
-    <PresetPlayer v-else />
-    <Alerts />
+<template>
+  <div class="flex">
+    <Button class="mx-auto">Hello World</Button>
   </div>
 </template>
 
--- web/src/main.js
+++ web/src/main.js
@@ -1,8 +1,14 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import store from "./store.js"
-import "./index.css";
+import { createApp } from 'vue'
+import PrimeVue from 'primevue/config';
+import App from './App.vue'
+import store from "./store"
+
+import "primevue/resources/themes/saga-blue/theme.css"
+import "primevue/resources/primevue.min.css"
+import "primeicons/primeicons.css"
+import "/node_modules/primeflex/primeflex.css"
 
 createApp(App)
+    .use(PrimeVue)
     .use(store)
-    .mount("#app");
+    .mount('#app');

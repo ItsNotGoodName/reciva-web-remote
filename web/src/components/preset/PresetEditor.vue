d5be19f1a80d9bd36e50ba5207c351e22a18d36a
ItsNotGoodName <gurnaindeol@gmail.com>
2021-11-09 18:53:51 -0800

    feat(web): add preset editor

--- web/src/components/preset/PresetEditor.vue
+++ web/src/components/preset/PresetEditor.vue
@@ -1,10 +1,82 @@
 <template>
-  <div>Hello world!</div>
+  <div class="p-fluid">
+    <div class="card">
+      <DataTable
+        :value="presets"
+        editMode="cell"
+        @cell-edit-complete="onCellEditComplete"
+        class="editable-cells-table"
+        responsiveLayout="scroll"
+      >
+        <Column
+          v-for="col of columns"
+          :field="col.field"
+          :header="col.header"
+          :key="col.field"
+          style="width: 25%"
+        >
+          <template v-if="col.field != 'url'" #editor="{ data, field }">
+            <InputText v-model="data[field]" autofocus />
+          </template>
+        </Column>
+      </DataTable>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+import { mapActions, mapState } from "vuex";
+
+import Column from "primevue/column";
+import DataTable from "primevue/datatable";
+import InputText from "primevue/inputtext";
+
+export default {
+  components: {
+    Column,
+    DataTable,
+    InputText,
+  },
+  data() {
+    return {
+      columns: null,
+    };
+  },
+  created() {
+    this.columns = [
+      {
+        field: "url",
+        header: "URL",
+      },
+      {
+        field: "newName",
+        header: "New Name",
+      },
+      {
+        field: "newUrl",
+        header: "New URL",
+      },
+    ];
+  },
+  methods: {
+    ...mapActions(["updatePreset"]),
+    onCellEditComplete(event) {
+      let { data, newValue, field } = event;
+      data[field] = newValue;
+      this.updatePreset(data);
+    },
+  },
+  computed: {
+    ...mapState({
+      presets: (state) => state.p.presets,
+    }),
+  },
+};
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+::v-deep(.editable-cells-table td.p-cell-editing) {
+  padding-top: 0;
+  padding-bottom: 0;
+}
 </style>

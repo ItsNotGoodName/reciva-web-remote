54672b099ed61196141bb2f13a3070627d928622
ItsNotGoodName <gurnaindeol@gmail.com>
2021-11-09 17:51:09 -0800

    refactor(web): change preset list

--- web/src/components/radio/PresetList.vue
+++ web/src/components/radio/PresetList.vue
@@ -5,23 +5,7 @@
       v-for="preset in radio.presets"
       :key="preset.number"
     >
-      <Button
-        v-if="preset.number == radio.preset"
-        :loading="loading"
-        :label="preset.name"
-        @click="playRadioPreset(preset.number)"
-        :badge="'' + preset.number"
-        class="w-full"
-      />
-      <Button
-        v-else
-        :loading="loading"
-        :label="preset.name"
-        @click="playRadioPreset(preset.number)"
-        :badge="'' + preset.number"
-        badgeClass="p-badge-info"
-        class="w-full p-button-secondary p-button-outlined"
-      />
+      <preset-button :radio="radio" :preset="preset" />
     </div>
   </div>
 </template>
@@ -29,37 +13,14 @@
 <script>
 import { mapState } from "vuex";
 
-import Button from "primevue/button";
+import PresetButton from "./PresetButton.vue";
 
 export default {
   components: {
-    Button,
+    PresetButton,
   },
-  data: () => ({
-    loading: false,
-  }),
   computed: {
     ...mapState(["radio"]),
-  },
-  methods: {
-    playRadioPreset(preset) {
-      if (this.loading) return;
-
-      this.loading = true;
-      this.$store
-        .dispatch("playRadioPreset", preset)
-        .then(() => {
-          this.loading = false;
-        })
-        .catch((err) => {
-          this.loading = false;
-          this.$toast.add({
-            severity: "error",
-            summary: err || "Error",
-            life: 3000,
-          });
-        });
-    },
   },
 };
 </script>

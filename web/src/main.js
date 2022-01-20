6473ed547fee6da0641965f21bab171363fa7a55
ItsNotGoodName <gurnaindeol@gmail.com>
2022-01-20 01:30:15 -0800

    build: fix scss charset warnings

--- web/src/main.js
+++ web/src/main.js
@@ -1,10 +1,10 @@
 import { createApp } from "vue";
 
+import "./scss/index.scss";
+
 import Toast from "vue-toastification";
 
 import App from "./App.vue";
 import store from "./store";
-
-import "./scss/index.scss";
 
 createApp(App).use(Toast).use(store).mount("#app");

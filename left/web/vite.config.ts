c88928a9e983d2d8c47af15912ce1b9aa19ae816
ItsNotGoodName <gurnaindeol@gmail.com>
2022-05-03 17:46:12 -0700

    feat(web): change icon and remove vite-plugin-pwa

--- left/web/vite.config.ts
+++ left/web/vite.config.ts
@@ -1,37 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    VitePWA({
-      includeAssets: ['favicon.svg', 'favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
-      manifest: {
-        name: 'Reciva Web Remote',
-        short_name: 'Remote',
-        description: 'Control your legacy Reciva based internet radios',
-        theme_color: '#ffffff',
-        icons: [
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          }
-        ]
-      }
-    })
-  ],
+  plugins: [vue()]
 })
